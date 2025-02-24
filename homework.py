1780299bb0725d282f9a2bd8d04f9e8763fa5ef0
Shankar-Git5 <shankarsistla123@gmail.com>
2025-02-24 13:28:09 +0530

    DAy - 1

---
+++ homework.py
@@ -0,0 +1,47 @@
+# code - 1
+
+# def greatest_of_three(num1, num2, num3):
+#     if num1 == num2 == num3:
+#         print("All numbers are equal")
+#     elif num1 >= num2 and num1 > num3:
+#         print(num1, " is greatest")
+#     elif num2 >= num1 and num2 > num3:
+#         print(num2, " is greatest")
+#     else:
+#         print(num3, " is greatest")
+# greatest_of_three(3, 3, 1)
+
+# code - 2 
+
+# year = int(input("Enter a year: "))
+# if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Leap Year")
+
+# code - 3
+
+# marks = int(input("Enter marks"))
+# if (marks >= 90 and marks <= 100):
+#     print("A ")
+# if (marks >= 80 and marks <= 90):
+#     print("B")
+# if (marks >= 70 and marks <= 80):
+#     print("C")
+# if (marks <= 70):
+#     print("Fail")
+
+# code - 4
+
+# char = input("Enter Character: ").lower()
+# li = ["a", "e", "i", "o", "u"]
+# if char in li:
+#     print("Vowel")
+# elif (char.isalpha()):
+#     print("Consonant")
+# else :
+#     print("Neither")
+
+
+
+
+
+
